97a2e1615f5dd3bb2e718d1eac907e18996f98f0
Ruth Dantas <80555245+ruthdevgpwr@users.noreply.github.com>
2021-03-19 14:22:23 -0300

    implementação de boas práticas

--- TEDs/TED_04.py
+++ TEDs/TED_04.py
@@ -1,13 +1,19 @@
-print('--- IDENTIFICAÇÃO DA CATEGORIA DO NADADOR ---')
-idade = int(input('Nadador, por favor, informe a sua idade para saber em qual categoria você se encaixa: '))
+idade = int(input('--- IDENTIFICAÇÃO DA CATEGORIA DO NADADOR --- \n\
+Nadador, por favor, informe a sua idade para saber em qual categoria você se encaixa: '))
 
-if idade >= 5 and idade <= 7:
-    print('Nadador, você se encaixa na seguinte categoria: INFANTIL A')
-elif idade >= 8 and idade <=10:
-    print('Nadador, você se encaixa na seguinte categoria: INFANTIL B')
-elif idade >= 11 and idade <=13:
-    print('Nadador, você se encaixa na seguinte categoria: JUVENIL A')
-elif idade >= 14 and idade <= 17:
-    print('Nadador, você se encaixa na seguinte categoria: JUVENIL B')
+tamanho_barra = 60
+print(tamanho_barra * '=')
+mensagem = 'Nadador, você se encaixa na seguinte categoria:'
+
+if 5 <= idade <= 7:
+    print(mensagem + 'INFANTIL A')
+elif 8 <= idade <= 10:
+    print(mensagem + 'INFANTIL B')
+elif 11 <= idade <=13:
+    print(mensagem + 'JUVENIL A')
+elif 14 <= idade <= 17:
+    print(mensagem + 'JUVENIL B')
 elif idade >= 18:
-    print('Nadador, você se encaixa na seguinte categoria: SÊNIOR')
+    print(mensagem + 'SÊNIOR')
+
+print(tamanho_barra * '=')
