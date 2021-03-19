310a1ed416e63a7894b3c184d937f696d42fed32
Ruth Dantas <80555245+ruthdevgpwr@users.noreply.github.com>
2021-03-19 20:01:18 -0300

    Add questão 04

---
+++ atividade-A04/Q04.py
@@ -0,0 +1,8 @@
+numero1 = float(input('Digite o 1º número: '))
+numero2 = float(input('Digite o 2º número: '))
+numero3 = float(input('Digite o 3º número: '))
+
+soma = numero2 + numero3
+
+if numero1 > soma:
+    print(f'O número {numero1} é maior que a soma entre o número {numero2} e o número {numero3}')
